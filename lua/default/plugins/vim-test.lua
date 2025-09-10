d201dc76b5bb520ac9e39006ab2be30d19870043
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-09-10 17:36:13 +0300

    Fix test mapping

--- lua/default/plugins/vim-test.lua
+++ lua/default/plugins/vim-test.lua
@@ -3,9 +3,9 @@ return {
   config = function()
     vim.g['test#strategy'] = 'neovim_sticky'
     vim.g['test#echo_command'] = 0
-    vim.keymap.set('n', '<leader>R', ':TestNearest<CR>', {silent = true, desc = 'Run nearest test'})
-    vim.keymap.set('n', '<leader>T', ':TestFile<CR>',    {silent = true, desc = 'Run test file'})
-    vim.keymap.set('n', '<leader>A', ':TestSuite<CR>',   {silent = true, desc = 'Run test suit'})
-    vim.keymap.set('n', '<leader>L', ':TestLast<CR>',    {silent = true, desc = 'Re-run last test'})
+    vim.keymap.set('n', '<leader>R', ':TestNearest<CR>', {noremap = true, silent = true, desc = 'Run nearest test'})
+    vim.keymap.set('n', '<leader>T', ':TestFile<CR>',    {noremap = true, silent = true, desc = 'Run test file'})
+    vim.keymap.set('n', '<leader>A', ':TestSuite<CR>',   {noremap = true, silent = true, desc = 'Run test suit'})
+    vim.keymap.set('n', '<leader>L', ':TestLast<CR>',    {noremap = true, silent = true, desc = 'Re-run last test'})
   end
 }
