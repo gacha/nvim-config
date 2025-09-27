806f10577580371dc1cb17575b9b7a46d416bc6c
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-09-27 18:38:31 +0300

    Uncomment Go

--- lua/default/plugins/go.nvim.lua
+++ lua/default/plugins/go.nvim.lua
@@ -1,22 +1,21 @@
 return {
-  -- 'ray-x/go.nvim',
-  -- dependencies = {  -- optional packages
-  --   'ray-x/guihua.lua',
-  --   'neovim/nvim-lspconfig',
-  --   'nvim-treesitter/nvim-treesitter',
-  -- },
-  -- config = function()
-  --   require('go').setup()
-  --   Run GoFormat on save
-  --   vim.api.nvim_create_autocmd("BufWritePre", {
-  --     pattern = "*.go",
-  --     callback = function()
-  --       require('go.format').gofmt()
-  --     end,
-  --     group = augroup "GoFormat",
-  --   })
-  -- end,
-  -- event = {'CmdlineEnter'},
-  -- ft = {'go', 'gomod'},
-  -- build = ':lua require("go.install").update_all_sync()'
+  'ray-x/go.nvim',
+  dependencies = {  -- optional packages
+    'ray-x/guihua.lua',
+    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('go').setup()
+    -- Run GoFormat on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require('go.format').gofmt()
+      end,
+    })
+  end,
+  event = {'CmdlineEnter'},
+  ft = {'go', 'gomod'},
+  build = ':lua require("go.install").update_all_sync()'
 }
