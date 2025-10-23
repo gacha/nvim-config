a4afb0b3b351476177d2780f9e8fa5115ae156af
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-10-23 18:40:32 +0300

    Do some theme changes

--- lua/default/plugins/gruvbox.nvim.lua
+++ lua/default/plugins/gruvbox.nvim.lua
@@ -1,8 +1,15 @@
 return {
   'ellisonleao/gruvbox.nvim', priority = 1000,
   config = function()
-    require("gruvbox").setup()
-    vim.opt.background = "dark" -- or "light" for light mode
+    require("gruvbox").setup({
+      italic = {
+        comments = true,
+        strings = false,
+        operators = false
+      },
+      contrast = "",
+      transparent_mode = false,
+    })
     vim.cmd("colorscheme gruvbox")
   end
 }
