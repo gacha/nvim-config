826bd77ae964e7b2666c6d9ade15258e497f16fe
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-11-03 14:38:22 +0200

    Better leap preview, excluded clutter

--- lua/default/plugins/leap.lua
+++ lua/default/plugins/leap.lua
@@ -1,3 +1,14 @@
 return {
   'ggandor/leap.nvim',
+  config = function()
+    local leap = require('leap')
+    leap.opts.preview = function (ch0, ch1, ch2)
+      return not (
+        ch1:match('%s')
+        or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+      )
+    end
+    vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+    vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+  end
 }
