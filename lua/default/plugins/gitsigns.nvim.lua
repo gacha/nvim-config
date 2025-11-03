2e699b78e69e6e12894abe3bb3f67ad08cf894b5
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-11-03 14:36:04 +0200

    Update mappings

--- lua/default/plugins/gitsigns.nvim.lua
+++ lua/default/plugins/gitsigns.nvim.lua
@@ -48,22 +48,8 @@ return {
         map('n', '<leader>hp', gitsigns.preview_hunk)
         map('n', '<leader>hi', gitsigns.preview_hunk_inline)
 
-        map('n', '<leader>hb', function()
-          gitsigns.blame_line({ full = true })
-        end)
-
-        map('n', '<leader>hd', gitsigns.diffthis)
-
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis('~')
-        end)
-
-        map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
-        map('n', '<leader>hq', gitsigns.setqflist)
-
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>tw', gitsigns.toggle_word_diff)
 
         -- Text object
         map({'o', 'x'}, 'ih', gitsigns.select_hunk)
