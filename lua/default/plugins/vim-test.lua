0302354f6246e035beba3b5b7787b4a1f23f7dde
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-11-10 10:10:35 +0200

    Add --fail-fast to Rspec tests

--- lua/default/plugins/vim-test.lua
+++ lua/default/plugins/vim-test.lua
@@ -7,5 +7,7 @@ return {
     vim.keymap.set('n', '<leader>T', ':TestFile<CR>',    {noremap = true, silent = true, desc = 'Run test file'})
     vim.keymap.set('n', '<leader>A', ':TestSuite<CR>',   {noremap = true, silent = true, desc = 'Run test suit'})
     vim.keymap.set('n', '<leader>L', ':TestLast<CR>',    {noremap = true, silent = true, desc = 'Re-run last test'})
+    -- Rspec
+    vim.g['test#ruby#rspec#options'] = '--fail-fast'
   end
 }
