c519e45e3c97e5e617715384fc75e2419a91fd82
Gatis Tomsons <gatis.tomsons@gmail.com>
2025-12-04 13:43:05 +0200

    Add ability to open Git commits in browser

--- lua/default/plugins/vim-fugutive.lua
+++ lua/default/plugins/vim-fugutive.lua
@@ -1,9 +1,13 @@
 return {
   'tpope/vim-fugitive',
+  dependencies =  {
+    'tpope/vim-rhubarb',
+  },
   config = function()
     vim.g.fugitive_summary_format = '%cs | %<(20,trunc)%an | %s'
 
-    -- Open git history for the current buffer
-    vim.keymap.set('n', '<leader><leader>g', '<cmd>Git log --no-merges -p %<cr>')
+    vim.keymap.set('n', '<leader>gh', '<cmd>Git log --no-merges -p %<cr>')
+    vim.keymap.set('n', '<leader>go', '<cmd>GBrowse<cr>')
+    vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>')
   end
 }
