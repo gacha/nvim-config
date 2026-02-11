cae5aa6ec7d3354da016656b7cbc857a605b147a
Gatis Tomsons <gatis.tomsons@gmail.com>
2026-02-11 18:43:59 +0200

    Remove gitsigns mapping as I don't use them

--- lua/default/plugins/gitsigns.nvim.lua
+++ lua/default/plugins/gitsigns.nvim.lua
@@ -4,56 +4,6 @@ return {
     "tpope/vim-fugitive",
   },
   config = function ()
-    require('gitsigns').setup {
-      on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal({']c', bang = true})
-          else
-            gitsigns.nav_hunk('next')
-          end
-        end)
-
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal({'[c', bang = true})
-          else
-            gitsigns.nav_hunk('prev')
-          end
-        end)
-
-        -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk)
-        map('n', '<leader>hr', gitsigns.reset_hunk)
-
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end)
-
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end)
-
-        map('n', '<leader>hS', gitsigns.stage_buffer)
-        map('n', '<leader>hR', gitsigns.reset_buffer)
-        map('n', '<leader>hp', gitsigns.preview_hunk)
-        map('n', '<leader>hi', gitsigns.preview_hunk_inline)
-
-        -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-
-        -- Text object
-        map({'o', 'x'}, 'ih', gitsigns.select_hunk)
-      end
-    }
+    require('gitsigns').setup {}
   end
 }
