648280e25faf5b19db90ce5710bfa04af801f8e2
Gatis Tomsons <gatis.tomsons@gmail.com>
2026-02-11 18:44:23 +0200

    Change the mini.surround mappings to not conflict

--- lua/default/plugins/mini.lua
+++ lua/default/plugins/mini.lua
@@ -3,5 +3,18 @@ return {
   version = false,
   config = function()
     require('mini.comment').setup()
+    require('mini.surround').setup({
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        add = 'as', -- Add surrounding in Normal and Visual modes
+        delete = 'ds', -- Delete surrounding
+        find = '', -- Find surrounding (to the right)
+        find_left = '', -- Find surrounding (to the left)
+        highlight = '', -- Highlight surrounding
+        replace = 'cs', -- Replace surrounding
+        suffix_last = '', -- Suffix to search with "prev" method
+        suffix_next = '', -- Suffix to search with "next" method
+      },
+    })
   end
 }
